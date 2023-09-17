250d9c53aa79aea893dc7e8cc4b1b2edd598c55b
Lyutik13 <lyutikov.viktor.96@mail.ru>
2023-09-17 22:10:28 +0300

    useState, useEffect, axios, dataBackend

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,5 @@
-// import axios from 'axios'
+import React from 'react'
+import axios from 'axios'
 
 import Home from '../Home/Home'
 import CoffeLine from '../CoffeLine/CoffeLine'
@@ -6,16 +7,30 @@ import Header from '../Header/Header'
 import Nav from '../Nav/Nav'
 
 // import {bd} from '../bd'
-import { arrBest } from '../bdBest'
+// import { arrBest } from '../bdBest'
 import styles from './App.module.scss'
 
 function App() {
+	const [itemsBest, setItemsBest] = React.useState([])
+	// const [itemsOll, setItemsOll] = React.useState([])
+
+	React.useEffect(() => {
+		async function fetcData() {
+			const itemsResponse = await axios.get(
+				'https://65043c13c8869921ae24bc9d.mockapi.io/best'
+			)
+
+			setItemsBest(itemsResponse.data)
+		}
+		fetcData()
+	}, [])
+
 	return (
 		<div className="wrapper">
 			<h1>Coffee house</h1>
 			<Header></Header>
 
-			<Home bd={arrBest}></Home>
+			<Home bd={itemsBest}></Home>
 
 			<footer className={styles.footer}>
 				<div className="container">
